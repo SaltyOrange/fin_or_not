dbcd1338942a811f8d35c32044bf79181b82f203
Matko <matkoburul@gmail.com>
2017-02-16 15:45:36 +0100

    Modified extract bz cnn script

--- standalone_scripts/extract_image_regions_by_cnn.py
+++ standalone_scripts/extract_image_regions_by_cnn.py
@@ -10,6 +10,7 @@ from inception2layer_net.predict import Predictor
 from utils import get_blob_bounding_boxes
 
 VALID_PREDICTIONS = [[0]]
+BOUNDING_BOX_EXPAND_PERC = 0.2
 
 if len(sys.argv) < 4:
     sys.exit("Usage: script.py localization_model_dir images_dir output_dir")
@@ -41,16 +42,24 @@ for i, image_path in enumerate(glob.glob(os.path.join(image_dir, '*.jpg'))):
         classmap_height, classmap_width = classmap.shape
         original_image_height, original_image_width, _ = original_image.shape
         width_ratio = original_image_width/classmap_width
-        height_ratio = original_image_height/classmap_height
+        height_ratio = original_image_height/classmap_height	
 
-        for box in bounding_boxes:
+        for j, box in enumerate(bounding_boxes):
             x, y, w, h = box
             # Bounding box coordinate translation
             x *= width_ratio
             w *= width_ratio
             y *= height_ratio
-            h *= height_ratio
+            h *= height_ratio   
 
-            image = Image.fromarray(original_image[y-1:y+h-1, x-1:x+h-1, :])
+            # Bounding box expansion
+            x = max(0, int(x - x*BOUNDING_BOX_EXPAND_PERC/2))
+	    y = max(0, int(y - y*BOUNDING_BOX_EXPAND_PERC/2))
+            w = min(original_image_width, int(w + w*BOUNDING_BOX_EXPAND_PERC/2))
+	    h = min(original_image_height, int(h + h*BOUNDING_BOX_EXPAND_PERC/2))
 
-            image.save("%s/%d.jpg" % (output_dir, i))
+            image = Image.fromarray(original_image[y:y+h, x:x+h, :])
+
+            image.save("%s/%d_%d.jpg" % (output_dir, i, j))
+
+            print "saved %s - %d time(s)" % (image_path, j+1)
